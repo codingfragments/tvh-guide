bcf306f74e6f3d69062fd9fd62a8664de39f5872
Stefan Marx <smarx@marxworld.net>
2022-04-08 10:22:31 -0400

    Split config into factory which will enable future update on cfg via API Call Fixes #37

--- src/lib/server/globals.ts
+++ src/lib/server/globals.ts
@@ -3,11 +3,15 @@
 import fs from "fs";
 import type { Settings } from "$lib/types/config";
 
-function loadCfg() {
-    const jsonData = fs.readFileSync('./static/config.json', 'utf8');
+function loadCfg(cfgFile='./static/config.json') {
+    const jsonData = fs.readFileSync(cfgFile, 'utf8');
     const jsonObj = JSON.parse(jsonData);
     console.log("Server loaded")
     return <Settings>jsonObj;
+}
+
+export function saveCfg(cfgFile='./static/config.json') {
+    console.log("🚀 ~ file: globals.ts ~ line 14 ~ saveCfg ~ cfgFile", cfgFile)
 }
 
 export const serverCfg = loadCfg();
