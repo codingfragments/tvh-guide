67b53e5491bc03cd23f74905d94ee92cf215fd20
Stefan Marx <stefan@marx-merten.de>
2022-04-15 22:09:23 +0200

    Added base redirect for root level access

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,15 @@
     import {media} from "$lib/client/state/global"
 </script>
 
+<script context="module">
+    export async function load() {
+        return {
+            status: 302,
+            redirect: "/app/now"
+        };
+    }
+</script>
+
 <h1 class=" text-xl font-bold underline bg-theme-background">Welcome to SvelteKit!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
