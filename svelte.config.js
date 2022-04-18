5607529a2f700100580b463d2cc25c107010916c
Stefan Marx <stefan@marx-merten.de>
2022-04-19 00:44:01 +0200

    fixed

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+const path = require('path');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
