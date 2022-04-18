d0604c7798abfcfa00e388a7ded252e97ee38877
Stefan Marx <stefan@marx-merten.de>
2022-04-19 00:40:10 +0200

    enable Svelte components, added aliases AND add Nav Storry

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,47 @@
+
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf",
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          // When you have splitted your css over multiple files
-          // and use @import('./other-styles.css')
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          // When using postCSS 8
-          implementation: require('postcss'),
-        },
-      },
-    }
-  ],
-  "framework": "@storybook/svelte",
-  "svelteOptions": {
-    "preprocess": import("../svelte.config.js").preprocess
-  }
-}
+	webpackFinal: async (config) => {
+		const svelteLoader = config.module.rules.find(
+			(r) => r.loader && r.loader.includes('svelte-loader')
+		);
+		svelteLoader.options.preprocess = require('svelte-preprocess')({});
+		config.module.rules.push({
+			test: [/\.stories\.js$/, /index\.js$/],
+			use: [require.resolve('@storybook/source-loader')],
+			include: [path.resolve(__dirname, '../src')],
+			enforce: 'pre'
+		});
+		config.resolve.alias = {
+			...config.resolve.alias,
+			$lib: path.resolve(__dirname, '../src/lib'),
+			$components: path.resolve(__dirname, '../src/lib/components')
+		};
+		return config;
+	},
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-svelte-csf',
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				cssLoaderOptions: {
+					// When you have splitted your css over multiple files
+					// and use @import('./other-styles.css')
+					importLoaders: 1
+				},
+				postcssLoaderOptions: {
+					// When using postCSS 8
+					implementation: require('postcss')
+				}
+			}
+		}
+	],
+	framework: '@storybook/svelte',
+	svelteOptions: {
+		preprocess: import('../svelte.config.js').preprocess
+	}
+};
