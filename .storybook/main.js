110073e6894d4af069cee2095c7a3ecad79f23cd
Stefan Marx <smarx@marxworld.net>
2022-04-19 08:49:12 +0200

    updated for new behaviour in storrybook 6.2+

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,21 @@
 
 const path = require('path');
+const sveltePreprocess = require('svelte-preprocess');
+
+
+// integrate with sveltekit like https://codingcat.dev/tutorial/integrating-storybook-with-sveltekit
+// enable support for typescript and svelte preprocessing https://github.com/storybookjs/storybook/issues/12754#issuecomment-708884287
 
 module.exports = {
 	webpackFinal: async (config) => {
-		const svelteLoader = config.module.rules.find(
-			(r) => r.loader && r.loader.includes('svelte-loader')
-		);
-		svelteLoader.options.preprocess = require('svelte-preprocess')({});
+		// https://github.com/storybookjs/storybook/issues/12754#issuecomment-708884287
+    // const svelteLoader = config.module.rules.find(
+		// 	(r) => r.loader && r.loader.includes('svelte-loader')
+		// );
+		// svelteLoader.options.preprocess = require('svelte-preprocess')({});
+    // END Support for typescript
+    // Note, this may be obsolete in upcomming versions regarding (https://github.com/storybookjs/storybook/issues/12754#issuecomment-808856923) and (https://github.com/storybookjs/storybook/tree/next/examples/svelte-kitchen-sink)
+
 		config.module.rules.push({
 			test: [/\.stories\.js$/, /index\.js$/],
 			use: [require.resolve('@storybook/source-loader')],
@@ -26,6 +35,7 @@ module.exports = {
 		'@storybook/addon-essentials',
 		'@storybook/addon-svelte-csf',
 		{
+      // Integrate with tailwindcss FROM https://stackoverflow.com/questions/65495912/storybook-tailwind-how-should-i-add-tailwind-to-storybook
 			name: '@storybook/addon-postcss',
 			options: {
 				cssLoaderOptions: {
@@ -41,7 +51,10 @@ module.exports = {
 		}
 	],
 	framework: '@storybook/svelte',
-	svelteOptions: {
-		preprocess: import('../svelte.config.js').preprocess
-	}
+	// svelteOptions: {
+	// 	preprocess: import('../svelte.config.js').preprocess
+	// }
+  svelteOptions: {
+    preprocess: sveltePreprocess(),
+  },
 };
