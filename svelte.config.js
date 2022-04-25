a4f0c1de7e0d5b3db817d025ee4fcda4bd797b6a
Stefan Marx <stefan@marx-merten.de>
2022-04-25 22:36:50 +0200

    added some links to Solution

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,7 @@ import path from "path"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
+	// alias handling like described https://codingcat.dev/tutorial/integrating-storybook-with-sveltekit
 	// for more information about preprocessors
 	vite: {
 		resolve: {
