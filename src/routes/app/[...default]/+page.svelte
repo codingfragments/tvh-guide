952cde6d7472c655fcbc177ca87e2841976c32c0
Stefan Marx <smarx@marxworld.net>
2022-12-03 19:45:20 -0800

    Added Typographie

--- src/routes/app/[...default]/+page.svelte
+++ src/routes/app/[...default]/+page.svelte
@@ -1,4 +1,6 @@
-<h3 class="underline p-28"> NOT YET3 IMPLEMENTED </h3>
-
-<button class="btn btn-primary">Button</button>
-<button class="btn ">Button</button>
+<div class="prose">
+	<h1 class="">HEAD</h1>
+	<h2 class="">NOPE</h2>
+	<h3 class="">LINES</h3>
+</div>
+<div>This is a simple text <span class="font-bold text-primary">With</span> some placeholder</div>
