cef81e638468c8c737387936ba352978f9db66b5
Stefan Marx <stefan@marx-merten.de>
2022-12-13 00:10:16 +0100

    Added ChannelLogo Component

---
+++ src/lib/components/epg/ChannelLogo.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { getUIDarkContext } from '$lib/client/state/layoutContext';
+
+	// Icon URL for full channel icon or picon path
+	export let icon = '';
+	// Picon prefix (make sure the right picon library is available as subdir of base)
+	// Example: big, big_light, default, default_light
+	// small, small_light, small_tile, xs, xs_light,xs_tile
+	enum Sizes {
+		md = 'default',
+		lg = 'big',
+		sm = 'small',
+		xs = 'xs'
+	}
+	export let piconPrefix: 'default' | 'xs' | 'big' | 'small' = 'default';
+	const isDark = getUIDarkContext();
+
+	let iconFullPrefix = '';
+	$: {
+		iconFullPrefix = piconPrefix;
+		if ($isDark) {
+			iconFullPrefix += '_light';
+		}
+	}
+	// Alternative Text for Image Tag
+	export let alt = '';
+
+	// picons BASE URl
+	export let piconBaseUrl: string;
+	let clazz = '';
+	export { clazz as class };
+
+	let iconUrl = '';
+	$: {
+		if (icon.startsWith('picon://')) {
+			iconUrl = `${piconBaseUrl}/${iconFullPrefix}/${icon.substring(8)}`;
+		} else iconUrl = icon;
+	}
+</script>
+
+<img src={iconUrl} {alt} class={clazz} />
