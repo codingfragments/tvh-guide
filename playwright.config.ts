c3c0321324e6dd82f333101e1a3c8604384090cd
Stefan Marx <stefan@marx-merten.de>
2023-01-19 17:41:36 +0100

    enabled static test execution to support repeatable playwrigth tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,15 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	use: {
+		viewport: { width: 1280, height: 720 },
+		headless: false
+	},
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build && npm run preview -- -m testing',
 		port: 4173
 	},
+
 	testDir: 'tests'
 };
 
