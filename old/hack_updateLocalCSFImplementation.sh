a45a15888a547e9fac6cfa1b452c4a259a964bf1
Stefan Marx <stefan@marx-merten.de>
2026-02-11 13:28:43 +0100

    remove old code

--- old/hack_updateLocalCSFImplementation.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -rf node_modules/@storybook/addon-svelte-csf/dist && cp -rf ../../web-dev/addon-svelte-csf/dist node_modules/@storybook/addon-svelte-csf
--- old/src/routes/api/v1/settings/ui/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { uiCfg } from '$lib/server/globals';
-
-import type { RequestHandler } from './$types';
-export const GET: RequestHandler = () => {
-	const resp = new Response();
-	resp.json;
-	return json(uiCfg);
-};
